9d3b644eefedeb80530f7c490bcdc00194f98cbf
unknown <frantsyzonok.illya@gmail.com>
2020-05-31 22:09:30 +0300

    WEB4ik

---
+++ lab_7/main/js/main.js
@@ -0,0 +1,14 @@
+document.getElementById("subscribeInput").value = getSavedValue("subscribeInput");
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+function getSavedValue(v) {
+    if (!localStorage.getItem(v)) {
+        return "";
+    }
+    return localStorage.getItem(v);
+}
